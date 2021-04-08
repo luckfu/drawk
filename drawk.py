c513a7dd892c4faf5dcc9d88ca718b351be97f86
luckfu <luckfu@gmail.com>
2021-04-08 10:37:52 +0800

    Update drawk.py
    增加 mplfinance 方式绘制k线图，无法交互，但是速度快

--- drawk.py
+++ drawk.py
@@ -1,12 +1,109 @@
+from pyecharts.globals import CurrentConfig, NotebookType,OnlineHostType
+CurrentConfig.NOTEBOOK_TYPE = NotebookType.JUPYTER_LAB
+#CurrentConfig.ONLINE_HOST = '/opt/luckfu/pyecharts-assets/assets' #'https://echarts.apache.org/examples/vendors/echarts/' #OnlineHostType.NOTEBOOK_HOST
 from pyecharts.charts import Kline,Scatter,Line,Grid,Bar,EffectScatter
 from pyecharts import options as opts
 from pyecharts.render import make_snapshot
 from pyecharts.globals import SymbolType #,ThemeType
 from pyecharts.commons.utils import JsCode
-from pyecharts.globals import CurrentConfig, NotebookType
-CurrentConfig.NOTEBOOK_TYPE = NotebookType.JUPYTER_LAB
 from snapshot_pyppeteer import snapshot
 from pyecharts.render import make_snapshot
+
+import mplfinance as mpf
+from cycler import cycler
+import matplotlib as mpl
+
+def mpf_plot(data,klines=[],vlines=[],area=[],width=20,height=8,legend=True,ktype='candle'):
+    '''
+    mplfinance 方式绘制k线图，无法交互，但是速度快
+    - data:  pandas.DataFrame          #columns包含 [u'open', u'close', u'low', u'high',u'volume’]
+    - klines: list 在k线区域现实的线
+    - vlines: list 在volume区域现实的线 
+    - area:   list 自定义显示区域
+    ’‘’
+    data=data.copy()
+    mpl.rcParams['axes.prop_cycle'] = cycler(
+    color=['dodgerblue', 'deeppink', 
+    'navy', 'teal', 'maroon', 'darkorange', 
+    'indigo'])
+    
+    if len(data)>500:
+        ktype='line'
+    
+    # 设置线宽
+    mpl.rcParams['lines.linewidth'] = .5
+    
+    mc = mpf.make_marketcolors(
+        up='red', 
+        down='green', 
+        edge='i', 
+        wick='black', 
+        volume='in', 
+        ohlc='i',
+        inherit=True)
+    
+    s = mpf.make_mpf_style(
+        base_mpf_style='yahoo',
+        rc={'font.size':16},
+        gridaxis='both', 
+        gridstyle='-', 
+        y_on_right=True, 
+        marketcolors=mc)
+    
+    k_plot=[]
+    for i in klines:
+        k_plot.append(mpf.make_addplot(data[i].values))
+        
+    for i in vlines:
+        k_plot.append(mpf.make_addplot(data[i].values,panel=1))
+        
+    panels=2
+    for i in range(panels,len(area)+panels):
+        if isinstance(area[i-panels],list):
+            #print(i)
+            for l in area[i-panels]:
+                k_plot.append(mpf.make_addplot(data[l], type='line',panel=i))
+        else:
+            #print(data[area[i-panels]])
+            k_plot.append(mpf.make_addplot(data[area[i-panels]], type='line',panel=i))
+    
+    for i in data.columns:
+        if i=='BUY': 
+            data['b_t']=[i[1]  if i[0]==True else None for i in data[['BUY','low']].values ]
+            k_plot.append(mpf.make_addplot(data['b_t'],scatter=True, markersize=100, marker='^', color='m'))
+        if i=='SELL':
+            data['s_t']=[i[1]  if i[0]==True else None for i in data[['SELL','high']].values ]
+            k_plot.append(mpf.make_addplot(data['s_t'],scatter=True, markersize=100, marker='v', color='b'))
+                      
+    fig, axes = mpf.plot(data=data,type=ktype,
+                     tight_layout=True,
+                     volume=True,
+                     addplot=k_plot,#style=s,
+                     style=s,
+                     figratio=(width, height),
+                     datetime_format='%Y-%m-%d',
+                     returnfig=True,
+                     figscale=1.5,
+                     xrotation=1
+                     )
+    fig.canvas.toolbar_visible = False
+    fig.canvas.header_visible = False
+    fig.canvas.resizable = True
+
+    if (len(klines) > 0) & legend: axes[0].legend(klines, loc='upper center',
+                                       ncol=(len(klines)),
+                                       fancybox=True)
+    if len(vlines) > 0: 
+        axes[2].legend(vlines, loc='best')
+        
+    for i in range(4,len(area)+4):
+        if isinstance(area[i-4],list):
+            axes[i].legend(area[i-4], loc='best')
+    
+    
+    axes[0].set_title(data.code[0]);
+    return fig, axes
+
 
 class KChartData:
     def __init__(self,name,df,freq='D',precision=2):
@@ -66,7 +163,7 @@ class KChartData:
                 ),
                 tooltip_opts=opts.TooltipOpts(
                     trigger="axis",
-                    axis_pointer_type="cross",
+                    axis_pointer_type="line",
                     background_color="rgba(245, 245, 245, 0.8)",
                     border_width=2,
                     border_color="#ccc",
@@ -110,21 +207,21 @@ class KChartData:
             
         if 'BUY' in self.data.columns:
             v1 = self.data[self.data['BUY']==True].index.strftime("%Y-%m-%d").tolist()
-            v2 = self.data[self.data['BUY']==True]['close']
+            v2 = self.data[self.data['BUY']==True]['low']
             es_buy = (
                 EffectScatter()
                 .add_xaxis(v1)
-                .add_yaxis("",v2,symbol=SymbolType.ARROW,is_selected = True)
+                .add_yaxis("",v2,symbol=SymbolType.ARROW )#,is_selected = True)
             )
             c.overlap(es_buy)
         
         if 'SELL' in self.data.columns:
             v1 = self.data[self.data['SELL']==True].index.strftime("%Y-%m-%d").tolist()
-            v2 = self.data[self.data['SELL']==True]['low']
+            v2 = self.data[self.data['SELL']==True]['high']
             es_sell = (
                 EffectScatter()
                 .add_xaxis(v1)
-                .add_yaxis("",v2)
+                .add_yaxis("",v2,symbol=SymbolType.RECT)
             )
             c.overlap(es_sell)
         
@@ -142,7 +239,7 @@ class KChartData:
             #.add_yaxis("Volume", self.data.volume.values.tolist(),stack="v_stack",category_gap=2,)
             .add_yaxis(
                 series_name="成交量", 
-                yaxis_data=db[['i','volume','f']].values.tolist(),
+                y_axis=db[['i','volume','f']].values.tolist(),
                 xaxis_index=0,
                 yaxis_index=1,
                 label_opts=opts.LabelOpts(is_show=False),
@@ -279,8 +376,8 @@ class KChartData:
                        [Field1,Field2,...] Dataframe中的字段名列表，将显示在一个区域
           width: int   #图表宽度 px
           height:int   #图表高度 px
-          klines:str   #K线区域显示的数据，Dataframe中的字段名，如MA...
-          vline: str   #Volume区域显示的数据，Dataframe中的字段名，如MA...
+          klines:list   #K线区域显示的数据，Dataframe中的字段名，如MA...
+          vline: list   #Volume区域显示的数据，Dataframe中的字段名，如MA...
         - sample:
            chart=data.plot(area=[['small_pct','medium_pct','big_pct','super_pct'],'V','cci'],vlines=['vMA5','vMA10'],klines=['MA5','MA10'])  
         '''
@@ -311,11 +408,11 @@ class KChartData:
             iButton=70
         icount=0
         for w in area:
-            #print(iStep)
+            print(w)
             if type(w)==list:
                 window = Line().add_xaxis(self.dateindex)
                 for l in w:
-                     window.add_yaxis(series_name=l, 
+                    window.add_yaxis(series_name=l, 
                         y_axis=round(self.data[l],self.precision).values.tolist(),
                         is_smooth=True,
                         is_symbol_show=False,
@@ -323,6 +420,17 @@ class KChartData:
                         label_opts=opts.LabelOpts(is_show=False),
                         linestyle_opts= opts.LineStyleOpts(type_='solid',width=2)
                     )
+                    #print('_'+ str(l)+'_flag')
+                    if '_'+ l+'_flag' in self.data.columns:
+                        print("    find_flag: %s" % '_'+ l+'_flag')
+                        xx = self.data[self.data['_'+ l+'_flag']==True].index.strftime("%Y-%m-%d").tolist()
+                        yy = self.data[self.data['_'+ l+'_flag']==True][l]
+                        c_flag = (
+                            EffectScatter()
+                            .add_xaxis(xx)
+                            .add_yaxis("",round(yy,self.precision))
+                        )
+                        window.overlap(c_flag)
                 window.axislabel_opts=opts.LabelOpts(is_show=False),
                 window.set_global_opts(datazoom_opts=[opts.DataZoomOpts()],
                             xaxis_opts=opts.AxisOpts(
@@ -354,6 +462,16 @@ class KChartData:
                             label_opts=opts.LabelOpts(is_show=False),
                             linestyle_opts= opts.LineStyleOpts(type_='solid',width=2)
                           )
+                    if '_'+ wi+'_flag' in self.data.columns:
+                        print("    find_flag: %s" % '_'+ wi+'_flag')
+                        v1 = self.data[self.data['_'+ wi+'_flag']==True].index.strftime("%Y-%m-%d").tolist()
+                        v2 = self.data[self.data['_'+ wi+'_flag']==True][wi]
+                        c_flag = (
+                            EffectScatter()
+                            .add_xaxis(v1)
+                            .add_yaxis("",round(v2,self.precision))
+                        )
+                        window.overlap(c_flag)
                 window.axislabel_opts=opts.LabelOpts(is_show=True),
                 window.set_global_opts(datazoom_opts=[opts.DataZoomOpts()],
                             xaxis_opts=opts.AxisOpts(
@@ -364,16 +482,7 @@ class KChartData:
                             legend_opts=opts.LegendOpts(orient='horizontal',pos_left=str(icount+20)+"%"),
                                        
                 )
-                if '_'+ w+'_flag' in self.data.columns:
-                    #print("find_flag")
-                    v1 = self.data[self.data['_'+ w+'_flag']==True].index.strftime("%Y-%m-%d").tolist()
-                    v2 = self.data[self.data['_'+ w+'_flag']==True][w]
-                    c_flag = (
-                        EffectScatter()
-                        .add_xaxis(v1)
-                        .add_yaxis("",v2)
-                    )
-                    window.overlap(c_flag)
+                
                 #grid.add(vLine,grid_opts=opts.GridOpts(pos_top= str(iButton)+'%',height=str(iStep)+'%')) 
             icount+=1
             #最后一行加上x刻度
@@ -387,3 +496,4 @@ class KChartData:
     
     def save_png(self,charts,filename):
         make_snapshot(snapshot, charts.render(),filename)
+    
